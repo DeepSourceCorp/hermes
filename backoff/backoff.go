9321e884538ea51ca28d53a76eb5dc2823d8367f
Vishnu Jayadevan <me@vishnujayadevan.com>
2022-01-11 13:30:48 +0530

    fix:add-backoff-interface

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -4,3 +4,8 @@ import "time"
 
 const defaultMin time.Duration = 100 * time.Millisecond
 const defaultMax time.Duration = 10 * time.Minute
+
+type Backoff interface {
+	Duration() time.Duration
+	ForAttempt(attempt float64) time.Duration
+}
