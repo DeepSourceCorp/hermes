1944fc50e13931ea96fdeed196f33042875c019d
Vishnu Jayadevan <vishnu@deepsource.io>
2022-04-13 18:05:40 +0530

    fix: Fix template configuration and add validators

--- config/app_test.go
+++ config/app_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestAppConfig_Validate(t *testing.T) {
 	type fields struct {
-		Port        int
-		TemplateDir string
+		Port               int
+		TemplateConfigPath string
 	}
 	tests := []struct {
 		name    string
@@ -16,25 +16,25 @@ func TestAppConfig_Validate(t *testing.T) {
 	}{
 		{
 			name:    "validate AppConfig",
-			fields:  fields{Port: 7272, TemplateDir: "./"},
+			fields:  fields{Port: 7272, TemplateConfigPath: "./"},
 			wantErr: false,
 		},
 		{
 			name:    "validate AppConfig with PORT 0",
-			fields:  fields{Port: 0, TemplateDir: "./"},
+			fields:  fields{Port: 0, TemplateConfigPath: "./"},
 			wantErr: true,
 		},
 		{
 			name:    "validate AppConfig with TemplateDir empty",
-			fields:  fields{Port: 7272, TemplateDir: ""},
+			fields:  fields{Port: 7272, TemplateConfigPath: ""},
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			config := &AppConfig{
-				Port:        tt.fields.Port,
-				TemplateDir: tt.fields.TemplateDir,
+				Port:               tt.fields.Port,
+				TemplateConfigPath: tt.fields.TemplateConfigPath,
 			}
 			if err := config.Validate(); (err != nil) != tt.wantErr {
 				t.Errorf("AppConfig.Validate() error = %v, wantErr %v", err, tt.wantErr)
